daa793590b4d2322d91bf7cc83f4c02acb77d13c
IssayTseng <issay@qq.com>
2019-01-16 17:05:48 +0800

    redis key serializer modify

--- src/main/java/com/oraclechain/eosio/EosBlockchainApplication.java
+++ src/main/java/com/oraclechain/eosio/EosBlockchainApplication.java
@@ -24,21 +24,31 @@ public class EosBlockchainApplication extends SpringBootServletInitializer {
 		SpringApplication.run(EosBlockchainApplication.class, args);
 	}
 
-	@Bean(name="redisTemplate")
-	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
-		RedisTemplate<String, String> template = new RedisTemplate<>();
-		RedisSerializer<String> redisSerializer = new StringRedisSerializer();
-		template.setConnectionFactory(factory);
-		//key序列化方式
-		template.setKeySerializer(redisSerializer);
-		//value序列化
-		template.setValueSerializer(redisSerializer);
-		//value hashmap序列化
-		template.setHashValueSerializer(redisSerializer);
-		//key haspmap序列化
-		template.setHashKeySerializer(redisSerializer);
-		//
-		return template;
+	@Bean
+	public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+		// 使用Jackson2JsonRedisSerialize 替换默认序列化
+		Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+		objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
+		jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+		// 设置value的序列化规则和 key的序列化规则
+		redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+		redisTemplate.setHashKeySerializer(jackson2JsonRedisSerializer);
+		redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+		redisTemplate.setDefaultSerializer(jackson2JsonRedisSerializer);
+		redisTemplate.setEnableDefaultSerializer(true);
+		redisTemplate.afterPropertiesSet();
+		return redisTemplate;
 	}
 
 
